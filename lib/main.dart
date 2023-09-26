4ab0ffd944d935f2ba49b5e789b4d3c98b2680cf
fury <enfnal24@gmail.com>
2023-09-26 20:02:45 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp()); // 앱 구동
 }
 
@@ -33,16 +35,16 @@ class MyApp extends StatelessWidget {
 
   // 버튼 위젯 TextButton() IconButton() ElevatedButton()
   // TextButton(child: Text('버튼'), onPressed: (){},)
-
+  //aaa
   Widget build(BuildContext context) {
     return MaterialApp(
       home : Scaffold(
         appBar: AppBar(
-          actions: [
-            Icon(Icons.star),Icon(Icons.star),
-          ],
-          leading: Icon(Icons.star),
-          title: Text('제목')
+            actions: [
+              Icon(Icons.star),Icon(Icons.star),
+            ],
+            leading: Icon(Icons.star),
+            title: Text('제목')
         ),
         body: Container(
           decoration: BoxDecoration(border: Border.all(color: Colors.black)),
@@ -53,35 +55,35 @@ class MyApp extends StatelessWidget {
               Container(  // 이미지 박스
                 decoration: BoxDecoration(border: Border.all(color: Colors.black)),
                 width:200, height:200,
-                child: Image.asset('1694703774.png'),
+                child: Image.asset('1234.jpg'),
               ),
               Container(  // 설명칸 박스
-                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                width: 400, height: 200,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(  // 상품이름
-                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                      width:380, height:70,
-                      child: Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함', style: TextStyle(fontSize:25),)
-                    ),
-                    Container(  // 위치정보
-                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                      width:380, height:30,
-                      child: Text('성동구 행당동 끌올 10분전')
-                    ),
-                    Container(  // 가격
-                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                      width:380, height:30,
-                      child: Text('210,000원', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
-                    ),
-                    Container(  // 좋아요 개수
-                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-                      width:380, height:40,
-                    ),
-                  ],
-                )
+                  decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                  width: 400, height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(  // 상품이름
+                          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                          width:380, height:70,
+                          child: Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함', style: TextStyle(fontSize:25),)
+                      ),
+                      Container(  // 위치정보
+                          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                          width:380, height:30,
+                          child: Text('성동구 행당동 끌올 10분전')
+                      ),
+                      Container(  // 가격
+                          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                          width:380, height:30,
+                          child: Text('210,000원', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
+                      ),
+                      Container(  // 좋아요 개수
+                        decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                        width:380, height:40,
+                      ),
+                    ],
+                  )
               ),
             ],
           ),
