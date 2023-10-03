29d08ab775f24a0090c2b44387ddaae94ad37482
fury <enfnal24@gmail.com>
2023-10-03 09:29:29 +0900

    chat practice

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  await Firebase.initializeApp();
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(const MyApp()); // 앱 구동
 }
 
@@ -37,82 +35,63 @@ class MyApp extends StatelessWidget {
 
   // 버튼 위젯 TextButton() IconButton() ElevatedButton()
   // TextButton(child: Text('버튼'), onPressed: (){},)
-  //aaa
+  //aaabbb
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: () async{
-            final f = FirebaseFirestore.instance;
-            f.collection('Profile').doc('aa').set({'abc??':'bcb'});
-            print("ok?");
-            },
-          child: Container(
-              padding: EdgeInsets.all(13),
-              decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.grey), borderRadius: BorderRadius.circular(12)),
-              child: Text('Send', style: TextStyle(fontSize: 25),)),
+    return MaterialApp(
+      home : Scaffold(
+        appBar: AppBar(
+            actions: [
+              Icon(Icons.star),Icon(Icons.star),
+            ],
+            leading: Icon(Icons.star),
+            title: Text('제목')
+        ),
+        body: Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(  // 이미지 박스
+                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                width:200, height:200,
+                child: Image.asset('1234.jpg'),
+              ),
+              Container(  // 설명칸 박스
+                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                width: 400, height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                Container(  // 상품이름
+                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                      width:380, height:70,
+                      child: Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함', style: TextStyle(fontSize:25),)
+                  ),
+                Container(  // 위치정보
+                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                      width:380, height:30,
+                      child: Text('성동구 행당동 끌올 10분전')
+                  ),
+                Container(  // 가격
+                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                      width:380, height:30,
+                      child: Text('210,000원', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
+                  ),
+                Container(  // 좋아요 개수
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                    width:380, height:40,
+                  ),
+                  ],
+                )
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
-// home : Scaffold(
-// appBar: AppBar(
-// actions: [
-// Icon(Icons.star),Icon(Icons.star),
-// ],
-// leading: Icon(Icons.star),
-// title: Text('제목')
-// ),
-// body: Container(
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// padding: EdgeInsets.all(10),
-// child: Row(
-// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-// children: [
-// Container(  // 이미지 박스
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// width:200, height:200,
-// child: Image.asset('1234.jpg'),
-// ),
-// Container(  // 설명칸 박스
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// width: 400, height: 200,
-// child: Column(
-// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-// children: [
-// Container(  // 상품이름
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// width:380, height:70,
-// child: Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함', style: TextStyle(fontSize:25),)
-// ),
-// Container(  // 위치정보
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// width:380, height:30,
-// child: Text('성동구 행당동 끌올 10분전')
-// ),
-// Container(  // 가격
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// width:380, height:30,
-// child: Text('210,000원', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
-// ),
-// Container(  // 좋아요 개수
-// decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-// width:380, height:40,
-// ),
-// ],
-// )
-// ),
-// ],
-// ),
-// ),
-// ),
-
-
-
 
 
 
